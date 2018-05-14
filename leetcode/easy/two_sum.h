5da02f4b9479f30fe47b133722150acb44db8a24
boddicheg <boddicheg@gmail.com>
2018-05-14 05:09:11 -0400

    Added naive version of algorithm for problem 1

--- leetcode/easy/two_sum.h
+++ leetcode/easy/two_sum.h
@@ -1,5 +1,23 @@
 // https://leetcode.com/problems/two-sum/description/
 
+// Bruteforce - Runtime: 170ms
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(size_t i = 0; i < nums.size(); ++i)
+        {
+            for(size_t j = 0; j < nums.size(); ++j)
+            {
+                if(nums.at(i) + nums.at(j) == target && i != j)
+                {
+                    return { i, j };
+                }
+            }
+        }
+    }
+};
+
+// Fast - Runtime: 9 ms
 class Solution
 {
 public:
