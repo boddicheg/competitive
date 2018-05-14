9a86b9395d652d0aa4d44007a568abaa17f84d88
boddicheg <boddicheg@gmail.com>
2018-05-14 04:55:42 -0400

    Cleanup

--- codingame/easy/chuck_norris.h
+++ codingame/easy/chuck_norris.h
@@ -18,7 +18,8 @@ int main()
     std::string result;
 
     std::string binaryRepresentation;
-    for(size_t i = 0; i < MESSAGE.size(); i++) {
+    for (size_t i = 0; i < MESSAGE.size(); i++)
+    {
         std::bitset<7> mes(MESSAGE.at(i));
         std::string binary = mes.to_string();
         binaryRepresentation.append(binary);
@@ -26,35 +27,38 @@ int main()
     std::vector<std::string> subSequences;
     std::string subSequence;
     char currentSymbol = binaryRepresentation.at(0);
-    for(size_t i = 0; i < binaryRepresentation.size(); i++) {
+    for (size_t i = 0; i < binaryRepresentation.size(); i++)
+    {
         auto symbol = binaryRepresentation.at(i);
-        if (symbol != currentSymbol) {
+        if (symbol != currentSymbol)
+        {
             subSequences.push_back(subSequence);
             subSequence.clear();
             subSequence += symbol;
             currentSymbol = symbol;
-        } else {
-            subSequence += symbol;
         }
+        else
+            subSequence += symbol;
     }
-    if (!subSequence.empty()) {
+    if (!subSequence.empty())
         subSequences.push_back(subSequence);
-    }
 
-    for(size_t i = 0; i < subSequences.size(); i++) {
+    for (size_t i = 0; i < subSequences.size(); i++)
+    {
         std::string s = subSequences.at(i);
-        if (s.at(0) == '1') {
+        if (s.at(0) == '1')
+        {
             result += "0 ";
             result += std::string(s.size(), '0');
         }
-        if (s.at(0) == '0') {
+        if (s.at(0) == '0')
+        {
             result += "00 ";
             result += std::string(s.size(), '0');
 
         }
-        if (i != subSequences.size() - 1) {
+        if (i != subSequences.size() - 1)
             result += " ";
-        }
 
     }
 
