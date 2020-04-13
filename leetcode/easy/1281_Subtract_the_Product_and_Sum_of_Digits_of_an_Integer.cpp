e9d3fd03bc0d169c9efc39e9828255285447ee6a
boddicheg <boddicheg@gmail.com>
2020-04-13 16:50:32 +0300

    1281. Subtract the Product and Sum of Digits of an Integer - 100% runtime/mem

---
+++ leetcode/easy/1281_Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+
+template<typename T>
+void print(const T &container)
+{
+    std::cout << "[ ";
+    for (const auto &e : container)
+        std::cout << e << " ";
+    std::cout << "]" << std::endl;
+}
+
+std::ostream &operator<<(std::ostream &ss, const std::pair<int, int> &p)
+{
+    ss << "[ " << p.first << ", " << p.second << " ]";
+    return ss;
+}
+
+template<typename T>
+std::ostream &operator<<(std::ostream &ss, const std::vector<T> &c)
+{
+    ss << "[ ";
+    for (const auto &e : c)
+        ss << e << " ";
+    ss << "]" << std::endl;
+    return ss;
+}
+
+static int x = []() { std::ios::sync_with_stdio(false); std::cin.tie(NULL); return 0; }();
+
+class Solution {
+public:
+    int subtractProductAndSum(int n)
+    {
+        int product = 1;
+        int sum = 0;
+        
+        while(n != 0)
+        {
+            int digit = n % 10;
+            product *= digit;
+            sum += digit;
+            n /= 10;
+        }
+
+        return product - sum;
+    }
+};
+int main(int argc, char const *argv[])
+{
+    Solution s;
+    auto result = s.subtractProductAndSum(234);
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
