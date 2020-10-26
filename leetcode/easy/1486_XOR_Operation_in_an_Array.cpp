9e9cba511448df9a1bfddc2642bc16cc5a8ae344
boddicheg <boddicheg@gmail.com>
2020-10-26 17:36:08 +0200

    1486. XOR Operation in an Array

---
+++ leetcode/easy/1486_XOR_Operation_in_an_Array.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+
+template<typename T>
+void print(const T &container)
+{
+    std::cout << "[ ";
+    for (const auto &e : container)
+        std::cout << e << " ";
+    std::cout << "]" << std::endl;
+}
+
+std::ostream &operator<<(std::ostream &ss, const std::pair<int, int> &p)
+{
+    ss << "[ " << p.first << ", " << p.second << " ]";
+    return ss;
+}
+
+template<typename T>
+std::ostream &operator<<(std::ostream &ss, const std::vector<T> &c)
+{
+    ss << "[ ";
+    for (const auto &e : c)
+        ss << e << " ";
+    ss << "]" << std::endl;
+    return ss;
+}
+
+static int x = []() { std::ios::sync_with_stdio(false); std::cin.tie(NULL); return 0; }();
+
+class Solution {
+public:
+    int xorOperation(int n, int start)
+    {
+        int result = 0;
+        int num = start;
+
+        for (size_t i = 1; i <= n; i ++)
+        {
+            result ^= num;
+            num = start +  2 * i;
+        }
+        return result;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution s;
+    auto result = s.xorOperation(10, 5);
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
